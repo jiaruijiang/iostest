669e7771e10a032c61bf2036e0025796ff0861b6
江嘉睿 <jiarui.jiang@castbox.fm>
2016-03-07 14:51:12 +0800

    initial template

---
+++ mytest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mytest
+//
+//  Created by 江嘉睿 on 16/3/7.
+//  Copyright © 2016年 江嘉睿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mytest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  mytest
+//
+//  Created by 江嘉睿 on 16/3/7.
+//  Copyright © 2016年 江嘉睿. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
